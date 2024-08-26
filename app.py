f6f36a80ae429d50e3b8d80f27b4d43cb5e2ec16
Mazan Aziz <mazanaziz420@gmail.com>
2024-08-26 04:28:12 +0000

    imported users bluepirnt routes in app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import JWTManager
 from flask_cors import CORS  # Import CORS
 from config import Config
 from models import init_app
+from routes.users_bp import users_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,6 +13,8 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 jwt = JWTManager(app)
 
 init_app(app)
+
+app.register_blueprint(users_bp, url_prefix='/')
 
 if __name__ == '__main__':
     app.run(debug=True)
