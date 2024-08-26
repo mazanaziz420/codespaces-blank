2c4536220d01b0f122183f5e24a27d75a0023633
Mazan Aziz <mazanaziz420@gmail.com>
2024-08-26 04:18:01 +0000

    COmmited configs required for app

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    MONGO_USERNAME=os.getenv('MONGO_USERNAME')
+    MONGO_PASSWORD=os.getenv('MONGO_PASSWORD')
+    MONGO_CLUSTER=os.getenv('MONGO_CLUSTER')
+    MONGO_AUTHSOURCE=os.getenv('MONGO_AUTHSOURCE')
+    MONGO_AUTHMECHANISM=os.getenv('MONGO_AUTHMECHANISM')
+    MONGO_URI='mongodb+srv://' + MONGO_USERNAME + ':' + MONGO_PASSWORD + '@' + MONGO_CLUSTER + '/eeve_db?authSource=' + MONGO_AUTHSOURCE + '&authMechanism=' + MONGO_AUTHMECHANISM
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    MAILJET_API_KEY = os.getenv('MAILJET_API_KEY')
+    MAILJET_SECRET_KEY = os.getenv('MAILJET_SECRET_KEY')
+    IMAGEKIT_PUBLIC_KEY = os.getenv('IMAGEKIT_PUBLIC_KEY')
+    IMAGEKIT_PRIVATE_KEY = os.getenv('IMAGEKIT_PRIVATE_KEY')
+    IMAGEKIT_URL_ENDPOINT = os.getenv('IMAGEKIT_URL_ENDPOINT')
+    
