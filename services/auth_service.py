8648d00529b39b4dd910214d467ff1ec3e999025
Mazan Aziz <mazanaziz420@gmail.com>
2024-08-26 04:23:37 +0000

    Added service for authentication of user

---
+++ services/auth_service.py
@@ -0,0 +1,11 @@
+from flask_jwt_extended import create_access_token as jwt_create_access_token
+from models import User
+
+def generate_access_token(email):
+    return jwt_create_access_token(identity=email)
+
+def authenticate_user(email, password):
+    user = User.find_by_email(email)
+    if user and User.verify_password(user['password'], password):
+        return user
+    return None
